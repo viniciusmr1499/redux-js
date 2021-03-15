0c9ba9b08b1d8f44ef9aeca5568e77c54eacc528
Marcos  Oliveira <viniciusmr1499@gmail.com>
2021-03-15 08:46:32 -0300

    fix redux

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button({ text }) {
+export default function Button(props, children) {
   return (
     <button type="button">
-      {text}
+      {children}
     </button>
   )
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addProduct } from '../../store/modules/cart/actions'
 
-import Button from '../Button'
-
 export default function Main() {
   const [product, setProduct] = useState('')
   const cart = useSelector(state => state.cart)
   const dispatch = useDispatch()
 
+  const handleAddProduct = (product) => {
+    dispatch(addProduct(product))
+    setProduct('')
+  }
+
   return (
     <>
       <input onChange={(e) => setProduct(e.target.value)} value={product} type="text" placeholder="Digite aqui o nome do produto" />
-      <Button onClick={() => dispatch(addProduct(product))} text="Adicionar produto" />  <br /> <br />
-      <Button text="Remover produto" />
+      <button type="button" onClick={() => handleAddProduct(product)}>
+        Adicionar Produto
+      </button>
+      <br/><br/>
+      <h1>Produtos</h1>
+      {cart.map(item => (
+        <h5 key={item}>- {item}</h5>
+      ))}
     </>
   )
 }
